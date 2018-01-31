7581cc023c9d5806a72b03d501a4f0a9448805ff
Lillian Chernin <l.smith1654@gmail.com>
2018-01-31 10:35:09 -0600

    complete step 3

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
 // Developer TODO: Define App component defintion here
 import React, {Component} from 'react';
+import Search from './Search';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <h1>Hello World!</h1>
+        <Search />
       </div>
     )
   }
---
+++ src/Search.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+        <button type="submit">Search</button>
+      </div>
+    )
+  }
+}
+
+export default Search;
