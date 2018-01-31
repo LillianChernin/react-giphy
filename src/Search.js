0737442b0d16650fac77dcfb437bd28c32ee7d9b
Lillian Chernin <l.smith1654@gmail.com>
2018-01-31 10:56:11 -0600

    complete step 4

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
 
 class Search extends Component {
+  constructor(){
+    super()
+    this.state = {
+      query: ''
+    }
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onInputChange(event) {
+    this.setState({
+      query: event.target.value
+    })
+  }
+  onSubmit() {
+    console.log(this.state.query);
+  }
   render() {
     return (
       <div>
-        <input type="text" />
-        <button type="submit">Search</button>
+        <input type="text" onChange={ this.onInputChange }/>
+        <button onClick={this.onSubmit}>Search</button>
       </div>
     )
   }
